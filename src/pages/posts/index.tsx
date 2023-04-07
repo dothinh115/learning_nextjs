95ac5e94a8f6bc246c3cc1ee9eacd22869db3375
nguyentrihau <nguyenchonhau@gmail.com>
2023-04-07 22:53:45 +0700

    up

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,39 @@
+import { GetStaticProps } from "next";
+import Link from "next/link";
 import React from "react";
 
-type Props = {};
-
-const index = (props: Props) => {
-  return <div>index</div>;
+type Props = {
+  posts: any[];
 };
 
-export default index;
+const postsList = ({ posts }: Props) => {
+  if (!posts) return;
+  return (
+    <>
+      <ul>
+        {posts.map((post: any) => {
+          return (
+            <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default postsList;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const response = await fetch(
+    "https://js-post-api.herokuapp.com/api/posts?_page=1"
+  );
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts: posts.data,
+    },
+  };
+};
